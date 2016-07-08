2ace7849799c126c856c0023128105429e7e099b
carlossru <carlos.znx43@gmail.com>
2016-07-08 09:48:33 -0500

    Tarea1
    Tarea 1 Curso Swift

--- Mini Reto.playground/Contents.swift
+++ Mini Reto.playground/Contents.swift
@@ -4,50 +4,16 @@ import UIKit
 
 for numero in 0...100 {
     
-    var entreCinco = (numero % 5)
-    var numPar = (numero % 2)
-    
-    if (entreCinco == 0) && (numPar == 0) {
-        switch numero {
-        case 30...40 :
-            print("\(numero) Bingo!! es Par!!, Viva Swift!!")
-            break
-        default :
-            print("\(numero) Bingo!! es Par!!")
-            break
-        }
+    if numero % 5 == 0 {
+        print("\(numero) Bingo!!")
     }
-    else if (entreCinco == 0) && (numPar != 0) {
-        switch numero {
-        case 30...40 :
-            print("\(numero) Bingo!! es Impar!!, Viva Swift!!")
-            break
-        default :
-            print("\(numero) Bingo!! es Impar!!")
-            break
-        }
+    if numero % 2 == 0 {
+        print("\(numero) Par!!")
+    } else {
+        print("\(numero) Impar!!")
     }
-    else if (numPar == 0) {
-        switch numero {
-        case 30...40 :
-            print("\(numero) es Par!!, Viva Swift!!")
-            break
-        default :
-            print("\(numero) es Par!!")
-            break
-        }
+    if (numero >= 30) && (numero <= 40) {
+        print("\(numero) Viva Swift!!")
     }
-    else {
-        switch numero {
-        case 30...40 :
-            print("\(numero) es Impar!!, Viva Swift!!")
-            break
-        default :
-            print("\(numero) es Impar!!")
-            break
-        }
-    }
-    
-    
     
 }
